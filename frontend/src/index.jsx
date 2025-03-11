e5251afbc6ef208d0821d7668fc073c38fda29cb
Андрей Сидорченко <andreisidorcenko@MacBook-Pro-Andrej.local>
2025-03-11 15:23:19 +0300

    Adding rollbar for control the errors in production

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import { I18nextProvider } from 'react-i18next';
 import { Provider } from 'react-redux';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
+import { useEffect } from 'react';
 import store from './redux/index.jsx';
 import initI18n from './initI18n.jsx';
 import App from './components/App';
@@ -8,17 +10,30 @@ import './index.css';
 import './assets/application.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import rollbarConfig from './rollbarConfig.js';
+
+const TestError = () => {
+  useEffect(() => {
+    throw new Error('Test Error');
+  }, []);
+  return null;
+};
 
 const app = async () => {
   const root = ReactDOM.createRoot(document.querySelector('#chat'));
   const i18n = await initI18n();
 
   root.render(
-    <Provider store={store}>
-      <I18nextProvider i18n={i18n}>
-        <App />
-      </I18nextProvider>
-    </Provider>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <Provider store={store}>
+          <I18nextProvider i18n={i18n}>
+            <App />
+            <TestError />
+          </I18nextProvider>
+        </Provider>
+      </ErrorBoundary>
+    </RollbarProvider>
   );
 };
 
