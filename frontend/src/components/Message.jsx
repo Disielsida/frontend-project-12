4dee2d442c3b4ed268d6c428a03b59c6218e574a
Андрей Сидорченко <andreisidorcenko@MacBook-Pro-Andrej.local>
2025-03-12 15:21:25 +0300

    Aria-label adding

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,11 +1,16 @@
 import { ListGroup } from 'react-bootstrap';
+import { useTransition } from 'react';
 
-const Message = ({ messageData }) => (
-  <ListGroup.Item className="text-break mb-2 border-0">
-    <strong>{messageData.username}</strong>
-    :&nbsp;
-    {messageData.body}
-  </ListGroup.Item>
-);
+const Message = ({ messageData }) => {
+  const { t } = useTransition();
+
+  return (
+    <ListGroup.Item className="text-break mb-2 border-0" aria-label={t('messageList.newMessage')}>
+      <strong>{messageData.username}</strong>
+      :&nbsp;
+      {messageData.body}
+    </ListGroup.Item>
+  );
+};
 
 export default Message;
