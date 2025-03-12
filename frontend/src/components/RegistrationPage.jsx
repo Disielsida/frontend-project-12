ea4b4de2b460c660cda1cb3a7234bb80d8bf3689
Андрей Сидорченко <andreisidorcenko@MacBook-Pro-Andrej.local>
2025-03-12 15:13:59 +0300

    Renaming of atributes for tests

--- frontend/src/components/RegistrationPage.jsx
+++ frontend/src/components/RegistrationPage.jsx
@@ -121,7 +121,7 @@ const RegistrationPage = () => {
                           && (!!formik.errors.password || !!networkError)
                         }
                       />
-                      <Form.Label htmlFor="суауау">{t('placeholders.password')}</Form.Label>
+                      <Form.Label htmlFor="password">{t('placeholders.password')}</Form.Label>
                       <Form.Control.Feedback type="invalid" className="invalid-tooltip">
                         {formik.errors.password}
                       </Form.Control.Feedback>
@@ -143,7 +143,7 @@ const RegistrationPage = () => {
                           && (!!formik.errors.confirmPassword || networkError)
                         }
                       />
-                      <Form.Label htmlFor="ConfirmPassword">{t('placeholders.confirmPassword')}</Form.Label>
+                      <Form.Label htmlFor="confirmPassword">{t('placeholders.confirmPassword')}</Form.Label>
                       <Form.Control.Feedback type="invalid" className="invalid-tooltip">
                         {formik.errors.confirmPassword || networkError}
                       </Form.Control.Feedback>
