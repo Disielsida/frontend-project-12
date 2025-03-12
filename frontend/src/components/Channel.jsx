ab37ab60e28afab5d549c4170aed40c61f9a35f5
Андрей Сидорченко <andreisidorcenko@MacBook-Pro-Andrej.local>
2025-03-12 14:44:57 +0300

    Changing atribut for channel name

--- frontend/src/components/Channel.jsx
+++ frontend/src/components/Channel.jsx
@@ -7,7 +7,7 @@ const BaseChannelButton = ({ channelData, activeChannelId, handleSetActiveChanne
     variant={activeChannelId === channelData.id ? 'secondary' : 'light'}
     className="w-100 rounded-0 text-start"
     onClick={() => handleSetActiveChannel(channelData.id)}
-    aria-label={channelData.name}
+    name={channelData.name}
   >
     <span className="me-1">#</span>
     {channelData.name}
